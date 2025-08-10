395d9dde83206a9c8189591589c2de53f2c85336
Rahul <rahulhanje0.7@gmail.com>
2025-08-10 11:47:00 +0530

    init

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from express
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app= express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
