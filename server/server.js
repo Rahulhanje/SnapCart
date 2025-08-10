a70afcca976e75dd258e77bfc8a4d5fdc417f696
Rahul <rahulhanje0.7@gmail.com>
2025-08-10 11:50:36 +0530

    server setup completed

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import express from express
+import express  from 'express'
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -9,4 +9,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the server!');
+});
 
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
